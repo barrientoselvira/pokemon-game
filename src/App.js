034ea17caa2460e910576c8b69860e217665c610
Elvira Barrientos <elvira@Elviras-MBP-59.lan>
2022-05-01 21:53:20 -0500

    pushing game up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+// import Pokecard from './Pokecard';
+// import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+
+class App extends Component {
+  render() {
+    return <div className="App">
+      <Pokegame />
+    </div>
+  };
+}
+
+export default App;
